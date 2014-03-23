c6b6607e2b78644cb4e7748745cf38bc45bdde0b
DarkGuana <Chris.Mecklenborg@gmail.com>
2014-03-23 13:30:36 -0500

    Upgraded version with loop checking

---
+++ src/node/hopper/rules/MultipleConditional.java
@@ -0,0 +1,41 @@
+package node.hopper.rules;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Dark Guana on 2014-03-23.
+ */
+public class MultipleConditional implements Conditional
+{
+  private List<Conditional> conditions;
+
+  public MultipleConditional(Conditional... conditions)
+  {
+    this.conditions = Arrays.asList(conditions);
+  }
+
+  @Override
+  public boolean isApplicable(Integer current, Integer target)
+  {
+    for (Conditional condition : conditions)
+    {
+      if(!condition.isApplicable(current, target))
+        return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String getDescription()
+  {
+    StringBuilder description = new StringBuilder();
+    for (Conditional condition : conditions)
+    {
+      description.append(condition.getDescription()).append(" and ");
+    }
+    //trim last and
+    description.setLength(description.length()-4);
+    return description.toString();
+  }
+}
--- src/node/hopper/scratchpad/Algorithm.java
+++ src/node/hopper/scratchpad/Algorithm.java
@@ -1,9 +1,9 @@
 package node.hopper.scratchpad;
 
-import node.hopper.rules.Conditional;
-import node.hopper.rules.PrioritizedConditionalRuleList;
-import node.hopper.rules.Rule;
-import node.hopper.rules.SimpleConditionalRule;
+import node.hopper.rules.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Dark Guana on 2014-03-22.
@@ -14,15 +14,23 @@ public class Algorithm
   {
     PrioritizedConditionalRuleList rule = new PrioritizedConditionalRuleList();
     rule.addNewConditional(new SimpleConditionalRule(getLessThan(), getMultiply(2)));
-    rule.addNewConditional(new SimpleConditionalRule(getMoreThan(), getDivide(3)));
+    rule.addNewConditional(new SimpleConditionalRule(new MultipleConditional(getDividableBy(3), getMoreThan()), getDivide(3)));
+    rule.addNewConditional(new SimpleConditionalRule(getMoreThan(0), getSubtract(1)));
 
     Integer current = 3;
-    Integer target = 5;
-    for (int i = 0 ; i < 10 && current != target; i++)
+    Integer target = 90;
+    Set<Integer> history = new HashSet<Integer>();
+    for (int i = 0 ; i < 100 && !current.equals(target) && current != null; i++)
     {
+      history.add(current);
       Integer next = rule.getNextValue(current, target);
       System.out.println(current + " -> " + next);
       current = next;
+      if (history.contains(next))
+      {
+        System.out.println("Loop detected");
+        break;
+      }
     }
   }
 
@@ -62,6 +70,42 @@ public class Algorithm
     };
   }
 
+  public static Conditional getMoreThan(final int check)
+  {
+    return new Conditional()
+    {
+      @Override
+      public boolean isApplicable(Integer current, Integer target)
+      {
+        return current > check;
+      }
+
+      @Override
+      public String getDescription()
+      {
+        return "val > " + check;
+      }
+    };
+  }
+
+  public static Conditional getDividableBy(final int div)
+  {
+    return new Conditional()
+    {
+      @Override
+      public boolean isApplicable(Integer current, Integer target)
+      {
+        return current % div == 0;
+      }
+
+      @Override
+      public String getDescription()
+      {
+        return "val divisible by "+div;
+      }
+    };
+  }
+
   public static Rule getMultiply(final int mod)
   {
     return new Rule()
@@ -94,6 +138,24 @@ public class Algorithm
       public String getDescription()
       {
         return "val / " + mod;
+      }
+    };
+  }
+
+  public static Rule getSubtract(final int mod)
+  {
+    return new Rule()
+    {
+      @Override
+      public Integer getNextValue(Integer current, Integer target)
+      {
+        return current - mod;
+      }
+
+      @Override
+      public String getDescription()
+      {
+        return "val - " + mod;
       }
     };
   }
